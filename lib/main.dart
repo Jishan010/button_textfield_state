f96cf2d5c4f0c87a7c021bdabb659eceaaee5fcf
Jishan Ansari <jishan010@gmail.com>
2022-11-10 19:46:22 +0530

    If applied, this commit will add state changes style to button and textfield

--- lib/main.dart
+++ lib/main.dart
@@ -20,26 +20,67 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//stateful widget with text field and button
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePage();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePage extends State<MyHomePage> {
+  final myController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  var isError = false;
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  Color getForegroundColor(Set<MaterialState> states) {
+    const Set<MaterialState> interactiveStates = <MaterialState>{
+      MaterialState.pressed,
+    };
+    if (states.contains(MaterialState.disabled)) {
+      return Colors.white;
+    } else if (states.any(interactiveStates.contains)) {
+      return Colors.white;
+    } else {
+      return Colors.white;
+    }
+  }
+
+  Color getBackgroundColor(Set<MaterialState> states) {
+    const Set<MaterialState> interactiveStates = <MaterialState>{
+      MaterialState.pressed,
+    };
+    if (states.contains(MaterialState.disabled)) {
+      return const Color(0xff97999B);
+    } else if (states.any(interactiveStates.contains)) {
+      return const Color(0xff004364);
+    } else {
+      return const Color(0xff005C8A);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    final forgroundColor =
+        MaterialStateProperty.resolveWith<Color>(getForegroundColor);
+
+    final backgroundColor =
+        MaterialStateProperty.resolveWith<Color>(getBackgroundColor);
+
+    final buttonStyle = ButtonStyle(
+      backgroundColor: backgroundColor,
+      foregroundColor: forgroundColor,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -48,21 +89,43 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(20),
+              child: TextFormField(
+                style: const TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        const BorderSide(color: Colors.deepPurple, width: 1.0),
+                  ),
+                  errorText: isError ? 'Please enter a valid text' : null,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Colors.green),
+                  ),
+                  hintText: 'Enter a search term',
+                ),
+                controller: myController,
+                onChanged: (text) {
+                  setState(() {
+                    isError = text == 'Error';
+                  });
+                },
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ElevatedButton(
+              style: buttonStyle,
+              onPressed: myController.text.isEmpty
+                  ? null
+                  : () {
+                      FocusScope.of(context).unfocus();
+                    },
+              child: const Text('Print Text'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
